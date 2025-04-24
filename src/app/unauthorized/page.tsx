959e730ed1c8f57dea6be69ddbef61a7383736b2
Hamid6426 <mianhamid6426@gmail.com>
2025-04-24 23:58:50 +0500

    unauthorized page

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -3,15 +3,16 @@ import Link from "next/link";
 // app/unauthorized/page.tsx
 export default function Unauthorized() {
   return (
-    <div className="h-screen flex items-center justify-center bg-red-50">
-      <div className="text-center">
-        <h1 className="text-3xl text-red-600 font-bold">Access Denied</h1>
-        <p className="text-gray-700 mt-2">You are not authorized to view this page.</p>
-        <p className="text-gray-700 mt-2">You are not Logged in Yet</p>
-        <Link className="bg-green-500 text-white py-2 px-4  hover:bg-green-600" href="/signin">
-          SignIn
+    <div className="absolute top-0 w-full h-screen flex flex-col gap-3 items-center justify-center bg-green-50 font-bold">
+        <h1 className="text-3xl text-red-600 ">Access Denied</h1>
+        <p className="text-gray-700 mt-4">You are not authorized to view this page.</p>
+        <Link className="bg-green-500 text-white py-2 px-4 hover:bg-green-600" href="/">
+          Home
         </Link>
-      </div>
+        <p className="text-gray-700 mt-4">If you are not Logged in Yet</p>
+        <Link className="bg-green-500 text-white py-2 px-4  hover:bg-green-600" href="/signin">
+          Click Here
+        </Link>
     </div>
   );
 }
